95a4c8d025ec92cf18d2b9ac970fbda17c4c57af
juanvel400 <juanvel400@proton.me>
2026-01-31 22:48:19 -0500

    allow the user to save the document as .md or .html

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, computed } from "vue";
 import { Marked } from "marked";
-const content = ref("");
+import DOMPurify from "dompurify";
 
-const html = ref("");
+const content = ref("");
+const html = computed(() => DOMPurify.sanitize(marked.parse(content.value)));
+
 const marked = new Marked({
   gfm: true,
 });
 
-watch(content, (newValue, oldValue) => {
-  html.value = marked.parse(newValue);
-});
+function saveText() {
+  const blob = new Blob([content.value], { type: "text/markdown" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "document.md";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
+function saveHTML() {
+  const blob = new Blob([html.value], { type: "text/html" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "document.html";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
 </script>
 
 <template>
@@ -18,9 +42,27 @@ watch(content, (newValue, oldValue) => {
     class="min-h-screen bg-neutral-300 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300 p-1"
   >
     <div
-      class="border rounded-md border-neutral-200 dark:border-neutral-800 p-2"
+      class="border rounded-md border-neutral-200 dark:border-neutral-800 p-2 flex items-center justify-between"
     >
-      <h1 class="font-bold animate-pulse">LiveMD</h1>
+      <h1 class="font-bold">
+        <span class="font-bold animate-pulse">LiveMD</span>
+        <span class="font-semibold"
+          >:
+          <i
+            >Minimalistic
+            <span title="What You See Is What You Get">WYSIWYG</span>
+            Markdown Editor</i
+          >
+        </span>
+      </h1>
+      <div class="font-bold">
+        <button type="button" @click="saveText" class="hover:underline me-2">
+          Save
+        </button>
+        <button type="button" @click="saveHTML" class="hover:underline">
+          Save as HTML
+        </button>
+      </div>
     </div>
     <div class="flex flex-1 gap-2 p-2 min-h-96">
       <textarea
