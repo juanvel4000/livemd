83cfc33f0c22bd1a1281321db7fb8af6ac45649d
juanvel400 <juanvel400@proton.me>
2026-01-31 20:14:53 -0500

    basic UI structure in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from "vue";
 
-<template></template>
+const content = ref("");
 
-<style scoped></style>
+const html = ref("");
+</script>
+
+<template>
+  <div
+    class="min-h-screen bg-neutral-300 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300 p-1"
+  >
+    <div
+      class="border rounded-md border-neutral-200 dark:border-neutral-800 p-2"
+    >
+      <h1 class="font-bold animate-pulse">LiveMD</h1>
+    </div>
+    <div class="flex flex-1 gap-2 p-2 min-h-96">
+      <textarea
+        v-model="content"
+        class="p-2 border border-neutral-200 dark:border-neutral-800 rounded-md flex-1 resize-none"
+      ></textarea>
+      <div
+        class="p-2 border rounded-md border-neutral-200 dark:border-neutral-800 flex-1 overflow-auto prose dark:prose-invert"
+        v-html="html"
+      ></div>
+    </div>
+  </div>
+</template>
